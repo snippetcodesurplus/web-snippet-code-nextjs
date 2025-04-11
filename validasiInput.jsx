b5e72d76c643d325ba69021e10196c101797189b
snippetcodesurplus <snippet.code00@gmail.com>
2025-04-11 09:18:05 +0700

    Update validasiInput.jsx

--- validasiInput.jsx
+++ validasiInput.jsx
@@ -12,18 +12,37 @@ const LoginForm = () => {
         const filteredValue = inputValue.replace(
             /[^0-9]/g, ''
         );
-
         e.target.value = filteredValue;
         // Set the filtered value as the input value
     };
   /* Akhir code const fungsi user hanya bisa input angka */
+
+  /* fungsi ini untuk user hanya bisa input alphabet */
+  const handleInputNama = (e) => {
+      const inputValue = e.target.value;
+      const filteredValue = inputValue.replace(
+          /[^a-zA-Z\s']/g, ''
+      );
+      e.target.value = filteredValue;
+  }
+  /* Akhir fungsi input alphabet */
   
   return (
-    <TextField
-      label="Hanya Angka"
-      variant="outlined"
-      onInput={handleInputHanyaAngka}
-    />
+    <div style={{
+      padding : '8px';
+    }}>
+      <TextField
+        label="Hanya Angka"
+        variant="outlined"
+        onInput={handleInputHanyaAngka}
+      />
+      <TextField
+        label="Hanya Alphabet"
+        variant="outlined"
+        onInput={handleInputNama}
+      />
+    </div>
+    
   )
 }
 
